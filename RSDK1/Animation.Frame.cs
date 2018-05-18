8fd87a6491349525eaf0765db4df16656336cec1
Rubberduckycooly <rubberduckycooly@outlook.com>
2018-05-19 00:18:55 +1000

    Early RSDKv1 Support

---
+++ RSDK1/Animation.Frame.cs
@@ -0,0 +1,132 @@
+﻿// MIT License
+// 
+// Copyright(c) 2017 Luciano (Xeeynamo) Ciccariello
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.IO;
+using RSDK;
+
+namespace RSDK1
+{
+    public class Frame : IFrame
+    {
+        public int SpriteSheet { get; set; }
+
+        public int CollisionBox { get; set; }
+
+        public int Id { get => 0; set { } }
+
+        public bool flag1 { get => false; set { } }
+
+        public bool flag2 { get => false; set { } }
+
+        public int Duration
+        {
+            get => 256;
+            set { }
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int CenterX { get; set; }
+
+        public int CenterY { get; set; }
+
+        public IHitbox GetHitbox(int index)
+        {
+            return new Hitbox();
+        }
+
+        public void SaveChanges(BinaryWriter writer)
+        {
+            Write(writer);
+        }
+
+        public void Read(BinaryReader reader)
+        {
+                // byte 1 - Number of image the frame is located in
+                // byte 2 - A flag mask of some kind
+                //		In Sonic 1 and Sonic 2, it has value 3 when crouching down, value 1 when jumping
+                //		In Sonic CD, it has value 1 for 3D Ramp 7, and value 2 for Size change,
+                //		and value 1 when jumping.
+                //		The bitmask most likely is composed by two bits.			
+                // byte 3 - X position in image of the frame
+                // byte 4 - Y position in image of the frame
+                // byte 5 - Width of frame
+                // byte 6 - Height of frame
+                // byte 7 - Hot spot horizontal displacement (signed)
+                // byte 8 - Hot spot vertical displacement (signed)				
+                SpriteSheet = reader.ReadByte();
+                int buf = reader.ReadByte();
+                flag1 = (buf & 1) > 0;
+                flag2 = (buf & 2) > 0;
+                X = reader.ReadByte();
+                Y = reader.ReadByte();
+                Width = reader.ReadByte();
+                Height = reader.ReadByte();
+                CenterX = reader.ReadSByte();
+                CenterY = reader.ReadSByte();
+                Id = 0;
+
+            /*SpriteSheet = reader.ReadByte();
+    CollisionBox = reader.ReadByte();
+    Id = 0;
+    X = reader.ReadByte();
+    Y = reader.ReadByte();
+    Width = reader.ReadByte();
+    Height = reader.ReadByte();
+    CenterX = reader.ReadSByte();
+    CenterY = reader.ReadSByte();*/
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write((byte)SpriteSheet);
+            writer.Write((byte)CollisionBox);
+            writer.Write((byte)X);
+            writer.Write((byte)Y);
+            writer.Write((byte)Width);
+            writer.Write((byte)Height);
+            writer.Write((byte)CenterX);
+            writer.Write((byte)CenterY);
+        }
+
+        public object Clone()
+        {
+            return new Frame()
+            {
+                SpriteSheet = SpriteSheet,
+                CollisionBox = CollisionBox,
+                X = X,
+                Y = Y,
+                Width = Width,
+                Height = Height,
+                CenterX = CenterX,
+                CenterY = CenterY
+            };
+        }
+    }
+}
---
+++ RSDK1/Animation.cs
@@ -0,0 +1,168 @@
+﻿// MIT License
+// 
+// Copyright(c) 2017 Luciano (Xeeynamo) Ciccariello
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using RSDK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RSDK1
+{
+    public class Animation : IAnimation
+    {
+        public int Version => 1;
+
+        public List<string> SpriteSheets { get; }
+
+        public List<AnimationEntry> Animations { get; }
+
+        public List<HitboxEntry> Hitboxes { get; }
+
+        public IEnumerable<string> HitboxTypes => null;
+
+        public Animation(BinaryReader reader)
+        {
+            // Read number of image paths		
+
+            reader.ReadByte(); //skip this byte, as i'm unsure what its for...
+            int spriteSheetsCount = 3; reader.ReadByte();
+            var animationsCount = reader.ReadByte();
+
+            SpriteSheets = new List<string>(spriteSheetsCount);
+
+            byte[] byteBuf = null;
+
+            for (int i = 0; i < spriteSheetsCount; i++)
+            {
+                int sLen = reader.ReadByte();
+                byteBuf = new byte[sLen];
+
+                byteBuf = reader.ReadBytes(sLen);
+
+                string result = System.Text.Encoding.UTF8.GetString(byteBuf);
+
+                SpriteSheets.Add(result);
+                Console.WriteLine(result);
+            }
+            byteBuf = null;
+            //while (spriteSheetsCount-- > 0)
+            //SpriteSheets.Add(StringEncoding.GetString(reader));
+
+            // Read number of animations		
+            Animations = new List<AnimationEntry>(animationsCount);
+
+            for (int i = 0; i < animationsCount; i++)
+            {// read frame count	
+                int frameCount = reader.ReadByte();
+                Console.WriteLine(frameCount);
+                		
+                int animationSpeed = reader.ReadByte();
+                animationSpeed = animationSpeed * 4;
+                int loopFrom = reader.ReadByte();
+                loopFrom = loopFrom + 1;
+
+                int buf = reader.ReadByte();
+
+                bool flag1 = (buf & 1) > 0;
+                bool flag2 = (buf & 2) > 0;
+
+                // Length of animation data - 4 bytes + (8 bytes * number_of_frames)
+
+                // In the 4 bytes:
+                // byte 1 - Number of frames
+                // byte 2 - Animation speed
+                // byte 3 - Frame to start looping from, when looping
+                // byte 4 - A flag of some kind
+                //		In Sonic 1, Sonic 2 and Sonic CD, it has value 3 for walking & running animations
+                //		Coincidentally, for those animations, the frames for the first half of the 
+                //		animation have the  normal graphics, while the second half 
+                //		has the rotated sprites
+                //		(that are displayed when going up a loop or a slope)
+                //		In Sonic 2, for Twirl H it has value 2
+
+                Animations.Add(new AnimationEntry(("Retro-Sonic Animation #" + (i+1)), frameCount, animationSpeed,
+                    loopFrom, flag1, flag2, reader));
+
+
+                }
+            }
+
+            public void Factory(out IAnimationEntry o) { o = new AnimationEntry(); }
+        public void Factory(out IFrame o) { o = new Frame(); }
+        public void Factory(out IHitboxEntry o) { o = new HitboxEntry(); }
+
+        public IEnumerable<IAnimationEntry> GetAnimations()
+        {
+            return Animations.Select(x => (IAnimationEntry)x);
+        }
+
+        public void SetAnimations(IEnumerable<IAnimationEntry> animations)
+        {
+            Animations.Clear();
+            Animations.AddRange(animations
+                .Select(x => x as AnimationEntry)
+                .Where(x => x != null));
+        }
+
+        public IEnumerable<IHitboxEntry> GetHitboxes()
+        {
+            return Hitboxes.Select(x => (IHitboxEntry)x);
+        }
+
+        public void SetHitboxes(IEnumerable<IHitboxEntry> hitboxes)
+        {
+            Hitboxes.Clear();
+            Hitboxes.AddRange(hitboxes
+                .Select(x => x as HitboxEntry)
+                .Where(x => x != null));
+        }
+        public void SetHitboxTypes(IEnumerable<string> hitboxTypes)
+        { }
+
+
+        public void SaveChanges(BinaryWriter writer)
+        {
+            var spriteSheetsCount = (byte)Math.Min(SpriteSheets.Count, byte.MaxValue);
+            writer.Write(spriteSheetsCount);
+            for (int i = 0; i < spriteSheetsCount; i++)
+            {
+                var item = SpriteSheets[i];
+                writer.Write(StringEncoding.GetBytes(item));
+            }
+
+            var animationsCount = (byte)Math.Min(Animations.Count, byte.MaxValue);
+            writer.Write(animationsCount);
+            for (int i = 0; i < animationsCount; i++)
+            {
+                Animations[i].SaveChanges(writer);
+            }
+
+            var collisionBoxesCount = (byte)Math.Min(Hitboxes.Count, byte.MaxValue);
+            writer.Write(collisionBoxesCount);
+            for (int i = 0; i < collisionBoxesCount; i++)
+            {
+                Hitboxes[i].SaveChanges(writer);
+            }
+        }
+    }
+}
